9aad67abb64e9f9df663e1579a535a7e7c9ee8f5
Mukhopadhyay <praneshmukherjee7@gmail.com>
2020-01-20 00:24:21 +0530

    adding intra xtacy results

--- script/results.js
+++ script/results.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
     $('#interResultButton').on('click', function() {
+        $('.wrapperIntra').css('display', 'none');
         if($('.wrapper').css('display')=='none') {
             $('.wrapper').css('display', 'block');
         }
         else {
             $('.wrapper').css('display', 'none');
+        }
+    })
+    $('#intraResultButton').on('click', function() {
+        $('.wrapper').css('display', 'none');
+        if($('.wrapperIntra').css('display')=='none') {
+            $('.wrapperIntra').css('display', 'block');
+        }
+        else {
+            $('.wrapperIntra').css('display', 'none');
         }
     })
 })
